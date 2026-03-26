e44a995247c9cd2fa3a74cfcf23b8ab9779c590a
Kahoj Yun <kahoj@users.noreply.github.com>
2026-03-26 15:38:26 +0800

    chore: ignore build artifacts

--- target/debug/deps/knope_playground-1430fdd6681c7066.d
+++
@@ -1,5 +0,0 @@
-/Users/kahoj/x/knope-playground/target/debug/deps/knope_playground-1430fdd6681c7066.d: src/lib.rs
-
-/Users/kahoj/x/knope-playground/target/debug/deps/knope_playground-1430fdd6681c7066: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/knope_playground-a1d78b9c5f8ff897.d
+++
@@ -1,7 +0,0 @@
-/Users/kahoj/x/knope-playground/target/debug/deps/knope_playground-a1d78b9c5f8ff897.d: src/lib.rs
-
-/Users/kahoj/x/knope-playground/target/debug/deps/libknope_playground-a1d78b9c5f8ff897.rlib: src/lib.rs
-
-/Users/kahoj/x/knope-playground/target/debug/deps/libknope_playground-a1d78b9c5f8ff897.rmeta: src/lib.rs
-
-src/lib.rs:
